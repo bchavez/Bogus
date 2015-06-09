e043d0eea8bf5af44fd9639f115a40db86c8d5f4
bchavez <bchavez@bitarmory.com>
2015-06-08 23:18:04 -0700

    * v2.4.1.0

--- Source/Bogus/Properties/AssemblyInfo.cs
+++ Source/Bogus/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyrightAttribute("Brian Chavez © 2015")]
 [assembly: AssemblyVersionAttribute("2.1.4.0")]
 [assembly: AssemblyFileVersion("2.1.4.0")]
-[assembly: AssemblyInformationalVersion("2.1.4.0 built on 6/9/2015 6:15:24 AM UTC")]
+[assembly: AssemblyInformationalVersion("2.1.4.0 built on 6/9/2015 6:17:28 AM UTC")]
 [assembly: AssemblyTrademark("MIT License")]
 [assembly: AssemblyDescriptionAttribute("http://www.github.com/bchavez/Bogus")]
 [assembly: ComVisible(false)]

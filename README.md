5a39e942243e37d92a8434b1a9847830f17012d6
bchavez <bchavez@bitarmory.com>
2015-06-10 21:36:10 -0700

    docs and more api

--- README.md
+++ README.md
@@ -47,29 +47,36 @@ var testOrders = new Faker<Order>()
 
 var userIds = 0;
 var testUsers = new Faker<User>()
-    // Optional: Call for objects that have complex initialization
+    //Optional: Call for objects that have complex initialization
     .CustomInstantiator(f => new User(userIds++, f.Random.Replace("###-##-####")))
 
-    // Basic rules using built-in generators
+    //Basic rules using built-in generators
     .RuleFor(u => u.FirstName, f => f.Name.FirstName())
     .RuleFor(u => u.LastName, f => f.Name.LastName())
     .RuleFor(u => u.Avatar, f => f.Internet.Avatar())
     .RuleFor(u => u.UserName, (f, u) => f.Internet.UserName(u.FirstName, u.LastName))
     .RuleFor(u => u.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
 
-    // Use an enum outside scope.
+    //Use an enum outside scope.
     .RuleFor(u => u.Gender, f => f.PickRandom<Gender>())
-    // Use a method outside scope.
+    //Use a method outside scope.
     .RuleFor(u => u.CartId, f => Guid.NewGuid())
-    // Compound property with context, use the first name + random last name
+    //Compound property with context, use the first/last name properties
     .RuleFor(u => u.FullName, (f, u) => u.FirstName + " " + u.LastName)
-    // And finally, composability of a complex collection.
-    .RuleFor(u => u.Orders, f => testOrders.Generate(3).ToList());
+    //And composability of a complex collection.
+    .RuleFor(u => u.Orders, f => testOrders.Generate(3).ToList())
+    //After all rules are applied finish with the following action
+    .FinishWith((f, u) =>
+        {
+            Console.WriteLine("User Created! Id={0}", u.Id);
+        });
 
 var user = testUsers.Generate();
 Console.WriteLine(user.DumpAsJson());
 
 /* OUTPUT:
+User Created! Id=0
+ * 
 {
   "Id": 0,
   "FirstName": "Audrey",
--- Source/Bogus.Tests/FluentTests.cs
+++ Source/Bogus.Tests/FluentTests.cs
@@ -27,29 +27,45 @@ namespace Bogus.Tests
                 //Pick some fruit from a basket
                 .RuleFor(o => o.Item, f => f.PickRandom(fruit))
                 //A random quantity from 1 to 10
-                .RuleFor(o => o.Quantity, f => f.Random.Number(1,10));
+                .RuleFor(o => o.Quantity, f => f.Random.Number(1, 10));
+
 
 
             var userIds = 0;
             var testUsers = new Faker<User>()
-                // Optional: Call for objects that have complex initialization
+                //Optional: Call for objects that have complex initialization
                 .CustomInstantiator(f => new User(userIds++, f.Random.Replace("###-##-####")))
 
-                // Basic rules using built-in generators
+                //Basic rules using built-in generators
                 .RuleFor(u => u.FirstName, f => f.Name.FirstName())
                 .RuleFor(u => u.LastName, f => f.Name.LastName())
                 .RuleFor(u => u.Avatar, f => f.Internet.Avatar())
                 .RuleFor(u => u.UserName, (f, u) => f.Internet.UserName(u.FirstName, u.LastName))
-                .RuleFor( u=> u.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
+                .RuleFor(u => u.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
 
-                // Use an enum outside scope.
+                //Use an enum outside scope.
                 .RuleFor(u => u.Gender, f => f.PickRandom<Gender>())
-                // Use a method outside scope.
+                //Use a method outside scope.
                 .RuleFor(u => u.CartId, f => Guid.NewGuid())
-                // Compound property with context, use the first name + random last name
+                //Compound property with context, use the first/last name properties
                 .RuleFor(u => u.FullName, (f, u) => u.FirstName + " " + u.LastName)
-                // And finally, composability of a complex collection.
-                .RuleFor(u => u.Orders, f => testOrders.Generate(3).ToList());
+                //And composability of a complex collection.
+                .RuleFor(u => u.Orders, f => testOrders.Generate(3).ToList())
+                //After all rules are applied finish with the following action
+                .FinishWith((f, u) =>
+                    {
+                        Console.WriteLine("User Created! Name={0}", u.FullName);
+                    });
+
+            //RunAfter
+            //InvokeAfterRules
+            //FinalizeWith
+            //AfterRulesInvoke
+            //PostProcess
+            //InvokeAfterAll
+            //BeforeReturn
+            //FinallyInvoke
+            //FinishWith
             
             var user = testUsers.Generate();
 

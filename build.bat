701f41a607d75a9adbe670fec1b2be22d261708d
bchavez <bchavez@bitarmory.com>
2015-06-11 09:52:58 -0700

    * Commit v2.1.4.2

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
-set version="2.1.4.1"
+set version="2.1.4.2"
 msbuild source\Builder\Builder.csproj
 Source\Builder\bin\Debug\fb.exe Source\Builder\bin\Debug\Builder.dll -c:LocaleBuildTask -p:Version="%version%" 
 Source\Builder\bin\Debug\fb.exe Source\Builder\bin\Debug\Builder.dll -c:ProjectBuildTask -p:Version="%version%"

aef03d802f3551aa45d8798e0af6f11d70b5a806
bchavez <bchavez@bitarmory.com>
2015-07-08 00:35:47 -0700

    * Migration to new FakerJS source data file format

--- Source/Bogus/locales/README.md
+++
@@ -1,8 +0,0 @@
-﻿## The Locales Directory
-
-
-#### Creating / Editing Locales
-
-Please edit the `.js` locale files, the `\data\*.json`
-files are automatically generated as a result
-of processing the `.js` files in this directory.
---
+++ Source/Builder/gulpfile.js
@@ -0,0 +1,41 @@
+﻿var gulp = require("gulp");
+var $ = require("gulp-load-plugins")({ lazy: true });
+var _ = require("underscore");
+
+var es = require("event-stream");
+
+//var File = require("vinyl");
+
+var locales = require("../fakerjs/lib/locales.js");
+
+var dataFolder = "../Bogus/data";
+
+gulp.task("build.locales", function() {
+
+  var localeCodes = _.keys(locales);
+
+  var files = _.map(localeCodes, function(code) {
+
+    var destName = code + ".locale.json";
+
+    var thisLocale = {};
+    thisLocale[code] = locales[code];
+
+    var localeFile = new $.util.File({
+      path: './' + destName,
+      contents: new Buffer(JSON.stringify(thisLocale, null, 2))
+    });
+
+    return localeFile;
+  });
+
+  return es.readArray(files)
+    .pipe($.print())
+    .pipe(gulp.dest(dataFolder));
+});
+
+
+//Helper Methods
+function log(msg) {
+  $.util.log($.util.colors.bgCyan(msg));
+};

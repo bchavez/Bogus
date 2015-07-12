0da6a013ce26e9c3f255bc1d2ac63f3f0ddebfd0
bchavez <bchavez@bitarmory.com>
2015-07-11 23:30:47 -0700

    nuget push 3.0.0.1

--- Source/Bogus/Properties/AssemblyInfo.cs
+++ Source/Bogus/Properties/AssemblyInfo.cs
@@ -1,14 +1,12 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitleAttribute("Bogus Fake Data Generator for .NET")]
 [assembly: AssemblyProductAttribute("Bogus")]
 [assembly: AssemblyCompanyAttribute("Brian Chavez")]
 [assembly: AssemblyCopyrightAttribute("Brian Chavez © 2015")]
-[assembly: AssemblyVersionAttribute("2.1.5.2")]
-[assembly: AssemblyFileVersion("2.1.5.2")]
-[assembly: AssemblyInformationalVersion("2.1.5.2 built on 6/22/2015 8:05:15 PM UTC")]
+[assembly: AssemblyVersionAttribute("3.0.0.1")]
+[assembly: AssemblyFileVersion("3.0.0.1")]
+[assembly: AssemblyInformationalVersion("3.0.0.1 built on 7/12/2015 6:29:05 AM UTC")]
 [assembly: AssemblyTrademark("MIT License")]
 [assembly: AssemblyDescriptionAttribute("http://www.github.com/bchavez/Bogus")]
 [assembly: ComVisible(false)]
-[assembly: InternalsVisibleTo("Bogus.Tests")]

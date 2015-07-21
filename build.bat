9c68798d891f4f9d48c3d7beaafb76513b027c21
Martijn Laarman <mpdreamz@gmail.com>
2015-07-21 16:50:16 +0200

    import vs env variables when running from vanilla cmd console

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
+@ECHO OFF
+
+IF NOT DEFINED DevEnvDir (
+	CALL "%vs120comntools%\vsvars32.bat"
+)
+
 set version="3.0.0.2"
 msbuild source\Builder\Builder.csproj
 Source\Builder\bin\Debug\fb.exe Source\Builder\bin\Debug\Builder.dll -c:ProjectBuildTask -p:Version="%version%"

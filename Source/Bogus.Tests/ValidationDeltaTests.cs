e532e0a1fdc4a25a505e9ed8cc6f930b49b62cc3
gdimauro <gdimauro@codearchitects.com>
2016-10-11 19:41:31 +0200

    more reverts

--- Source/Bogus.Tests/ValidationDeltaTests.cs
+++ Source/Bogus.Tests/ValidationDeltaTests.cs
@@ -4,79 +4,79 @@ using NUnit.Framework;
 
 namespace Bogus.Tests
 {
-  [TestFixture]
-  public class ValidationDeltaTests
-  {
-    [Test]
-    public void should_be_valid_and_not_throw_exception_on_complete_rules_with_ignore()
+    [TestFixture]
+    public class ValidationDeltaTests
     {
-      var testOrders = new Faker<Examples.Order>()
-          .StrictMode(true)
-          .Ignore(o => o.Item)
-          .RuleFor(o => o.OrderId, f => 3343)
-          .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5));
-      var result = testOrders.Validate();
-      Assert.DoesNotThrow(() => testOrders.AssertConfigurationIsValid());
-      result.ShouldBeEquivalentTo(true);
-    }
+        [Test]
+        public void should_be_valid_and_not_throw_exception_on_complete_rules_with_ignore()
+        {
+            var testOrders = new Faker<Examples.Order>()
+                .StrictMode(true)
+                .Ignore(o => o.Item)
+                .RuleFor(o => o.OrderId, f => 3343)
+                .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5));
+            var result = testOrders.Validate();
+            Assert.DoesNotThrow(() => testOrders.AssertConfigurationIsValid());
+            result.ShouldBeEquivalentTo(true);
+        }
 
-    [Test]
-    public void should_not_be_valid_and_throw_exception_on_incomplete_rules()
-    {
-      var testOrders = new Faker<Examples.Order>()
-          .StrictMode(true)
-          .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5));
-      var result = testOrders.Validate();
-      Assert.Throws<MissingFieldException>(() => testOrders.AssertConfigurationIsValid());
-      result.ShouldBeEquivalentTo(false);
-    }
+        [Test]
+        public void should_not_be_valid_and_throw_exception_on_incomplete_rules()
+        {
+            var testOrders = new Faker<Examples.Order>()
+                .StrictMode(true)
+                .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5));
+            var result = testOrders.Validate();
+            Assert.Throws<MissingFieldException>(() => testOrders.AssertConfigurationIsValid());
+            result.ShouldBeEquivalentTo(false);
+        }
 
-    [Test]
-    public void should_throw_exception_on_incomplete_rules()
-    {
-      var testOrders = new Faker<Examples.Order>()
-          .StrictMode(true)
-          .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5));
-      Assert.Throws<MissingFieldException>(() => testOrders.AssertConfigurationIsValid());
-    }
+        [Test]
+        public void should_throw_exception_on_incomplete_rules()
+        {
+            var testOrders = new Faker<Examples.Order>()
+                .StrictMode(true)
+                .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5));
+            Assert.Throws<MissingFieldException>(() => testOrders.AssertConfigurationIsValid());
+        }
 
-    [Test]
-    public void should_be_valid_and_no_excpetions_on_complete_rules()
-    {
-      var testOrders = new Faker<Examples.Order>()
-          .StrictMode(true)
-          .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5))
-          .RuleFor(o => o.OrderId, f => f.Random.Number(2, 5))
-          .RuleFor(o => o.Item, f => f.Lorem.Sentence())
-          ;
-      var result = testOrders.Validate();
-      Assert.DoesNotThrow(() => testOrders.AssertConfigurationIsValid());
-      result.ShouldBeEquivalentTo(true);
-    }
+        [Test]
+        public void should_be_valid_and_no_excpetions_on_complete_rules()
+        {
+            var testOrders = new Faker<Examples.Order>()
+                .StrictMode(true)
+                .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5))
+                .RuleFor(o => o.OrderId, f => f.Random.Number(2, 5))
+                .RuleFor(o => o.Item, f => f.Lorem.Sentence())
+                ;
+            var result = testOrders.Validate();
+            Assert.DoesNotThrow(() => testOrders.AssertConfigurationIsValid());
+            result.ShouldBeEquivalentTo(true);
+        }
 
-    [Test]
-    public void should_be_valid_no_excpetions_on_incomplete_rules_when_strict_false()
-    {
-      var testOrders = new Faker<Examples.Order>()
-          .StrictMode(false)
-          .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5));
-      var result = testOrders.Validate();
-      Assert.DoesNotThrow(() => testOrders.AssertConfigurationIsValid());
-      result.ShouldBeEquivalentTo(true);
-    }
+        [Test]
+        public void should_be_valid_no_excpetions_on_incomplete_rules_when_strict_false()
+        {
+            var testOrders = new Faker<Examples.Order>()
+                .StrictMode(false)
+                .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5));
+            var result = testOrders.Validate();
+            Assert.DoesNotThrow(() => testOrders.AssertConfigurationIsValid());
+            result.ShouldBeEquivalentTo(true);
+        }
 
-    [Test]
-    public void should_be_valid_on_complete_rules()
-    {
-      var testOrders = new Faker<Examples.Order>()
-          .StrictMode(true)
-          .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5))
-          .RuleFor(o => o.OrderId, f => f.Random.Number(2, 5))
-          .RuleFor(o => o.Item, f => f.Lorem.Sentence())
-          ;
-      var result = testOrders.Validate();
-      Assert.DoesNotThrow(() => testOrders.AssertConfigurationIsValid());
-      result.ShouldBeEquivalentTo(true);
+        [Test]
+        public void should_be_valid_on_complete_rules()
+        {
+            var testOrders = new Faker<Examples.Order>()
+                .StrictMode(true)
+                .RuleFor(o => o.Quantity, f => f.Random.Number(2, 5))
+                .RuleFor(o => o.OrderId, f => f.Random.Number(2, 5))
+                .RuleFor(o => o.Item, f => f.Lorem.Sentence())
+                ;
+            var result = testOrders.Validate();
+            Assert.DoesNotThrow(() => testOrders.AssertConfigurationIsValid());
+            result.ShouldBeEquivalentTo(true);
+        }
     }
-  }
 }

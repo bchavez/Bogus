2cee958be4d0308ba7792906cccc258525eeecc7
Luiz Guilherme Bauer Fraga Moreira <luiz.moreira@avalara.com>
2017-09-13 19:47:19 -0300

    Implement method to generate brazilian company document CNPJ (#96)

--- Source/Bogus/Extensions/ExtensionsForBrazil.cs
+++ Source/Bogus/Extensions/ExtensionsForBrazil.cs
@@ -1,57 +1,87 @@
 ﻿using System;
 using System.Linq;
+using Bogus.DataSets;
 
 namespace Bogus.Extensions.Brazil
 {
-   public static class ExtensionsForBrazil
-   {
-      private static Randomizer r = new Randomizer();
-      private static int[] Weights = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+    public static class ExtensionsForBrazil
+    {
+        private static Randomizer r = new Randomizer();
+        private static readonly int[] CpfWeights = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        private static readonly int[] CnpjWeights = { 2, 3, 4, 5, 6, 7, 8, 9, 2, 3, 4, 5, 6 };
 
-      /// <summary>
-      /// Cadastro de Pessoas Físicas
-      /// </summary>
-      public static string Cpf(this Person p)
-      {
-         const string Key = nameof(ExtensionsForBrazil) + "CPF";
-         if (p.context.ContainsKey(Key))
-         {
-            return p.context[Key] as string;
-         }
+        /// <summary>
+        /// Cadastro de Pessoas Físicas
+        /// </summary>
+        public static string Cpf(this Person p)
+        {
+            const string Key = nameof(ExtensionsForBrazil) + "CPF";
+            if (p.context.ContainsKey(Key))
+            {
+                return p.context[Key] as string;
+            }
 
-         var digits = r.Digits(9);
-         var sum1 = digits.Zip(Weights, (d, w) => d * w)
-             .Sum();
+            var digits = r.Digits(9);
+            var sum1 = digits.Zip(CpfWeights, (d, w) => d * w)
+                .Sum();
 
-         var sum1mod = sum1 % 11;
+            var sum1mod = sum1 % 11;
 
-         int check1 = 0;
-         if (sum1mod >= 2)
-         {
-            check1 = 11 - sum1mod;
-         }
+            int check1 = 0;
+            if (sum1mod >= 2)
+            {
+                check1 = 11 - sum1mod;
+            }
 
-         var finalWeights = new[] { 11 }.Concat(Weights);
+            var finalWeights = new[] { 11 }.Concat(CpfWeights);
 
-         var sum2 = digits.Concat(new[] { check1 })
-             .Zip(finalWeights, (d, w) => d * w)
-             .Sum();
+            var sum2 = digits.Concat(new[] { check1 })
+                .Zip(finalWeights, (d, w) => d * w)
+                .Sum();
 
-         var sum2mod = sum2 % 11;
+            var sum2mod = sum2 % 11;
 
-         var check2 = 0;
-         if (sum2mod >= 2)
-         {
-            check2 = 11 - sum2mod;
-         }
+            var check2 = 0;
+            if (sum2mod >= 2)
+            {
+                check2 = 11 - sum2mod;
+            }
 
-         var all = digits.Concat(new[] { check1, check2 }).ToArray();
+            var all = digits.Concat(new[] { check1, check2 }).ToArray();
 
-         var final = $"{all[0]}{all[1]}{all[2]}.{all[3]}{all[4]}{all[5]}.{all[6]}{all[7]}{all[8]}-{all[9]}{all[10]}";
+            var final = $"{all[0]}{all[1]}{all[2]}.{all[3]}{all[4]}{all[5]}.{all[6]}{all[7]}{all[8]}-{all[9]}{all[10]}";
 
-         p.context[Key] = final;
+            p.context[Key] = final;
 
-         return final;
-      }
-   }
+            return final;
+        }
+
+        /// <summary>
+        /// Cadastro Nacional da Pessoa Jurídica
+        /// </summary>
+        /// <returns></returns>
+        public static string Cnpj(this Company c)
+        {
+            var digits = r.Digits(12);
+            digits[8] = 0;
+            digits[9] = 0;
+            digits[10] = 0;
+            digits[11] = 1;
+
+            var firstDigit = digits.Reverse().Zip(CnpjWeights, (d, w) => d * w).Sum();
+            firstDigit = 11 - firstDigit % 11;
+
+            if (firstDigit >= 10)
+                firstDigit = 0;
+
+            var secondDigit = firstDigit * 2 + digits.Reverse().Zip(CnpjWeights.Skip(1), (d, w) => d * w).Sum();
+            secondDigit = 11 - (secondDigit % 11);
+            if (secondDigit >= 10)
+                secondDigit = 0;
+
+            var all = digits.Concat(new[] {firstDigit, secondDigit}).ToArray();
+            var final = $"{all[0]}{all[1]}.{all[2]}{all[3]}{all[4]}.{all[5]}{all[6]}{all[7]}/{all[8]}{all[9]}{all[10]}{all[11]}-{all[12]}{all[13]}";
+            return final;
+        }
+    }
 }

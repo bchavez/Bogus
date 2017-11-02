1a283286ffa56520b80959e64df9eb8e6246c18e
bchavez <bchavez@bitarmory.com>
2017-11-02 10:45:47 -0700

    Unit test coverage for Issue #99.

---
+++ Source/Bogus.Tests/GitHubIssues/Issue99.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus.DataSets;
+using FluentAssertions;
+using Xunit;
+
+namespace Bogus.Tests.GitHubIssues
+{
+   //https://github.com/bchavez/Bogus/issues/99
+   public class Issue99
+   {
+      [Fact]
+      public void multi_threaded_locale_access_should_be_okay()
+      {
+         ParallelEnumerable.Range(0, 9999)
+            .Select(i => new Name("nl")).ToList();
+      }
+   }
+}

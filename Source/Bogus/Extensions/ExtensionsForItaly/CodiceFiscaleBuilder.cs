9f0244e8feffe2a8ebb5da1fea2534b8da5333d4
supix <esposito.marce@gmail.com>
2017-11-24 21:49:41 +0100

    Cosmetic changes
    CodeMaid code reorganization run on `CodiceFiscaleBuilder.cs`.

--- Source/Bogus/Extensions/ExtensionsForItaly/CodiceFiscaleBuilder.cs
+++ Source/Bogus/Extensions/ExtensionsForItaly/CodiceFiscaleBuilder.cs
@@ -14,6 +14,97 @@ namespace Bogus.Extensions
     internal static class CodiceFiscaleBuilder
     {
         /// <summary>
+        /// Map used by the algoritm for even characters
+        /// </summary>
+        private static readonly Dictionary<char, int> evenMap = new Dictionary<char, int>
+        {
+            { '0', 0 },
+            { '1', 1 },
+            { '2', 2 },
+            { '3', 3 },
+            { '4', 4 },
+            { '5', 5 },
+            { '6', 6 },
+            { '7', 7 },
+            { '8', 8 },
+            { '9', 9 },
+            { 'A', 0 },
+            { 'B', 1 },
+            { 'C', 2 },
+            { 'D', 3 },
+            { 'E', 4 },
+            { 'F', 5 },
+            { 'G', 6 },
+            { 'H', 7 },
+            { 'I', 8 },
+            { 'J', 9 },
+            { 'K', 10 },
+            { 'L', 11 },
+            { 'M', 12 },
+            { 'N', 13 },
+            { 'O', 14 },
+            { 'P', 15 },
+            { 'Q', 16 },
+            { 'R', 17 },
+            { 'S', 18 },
+            { 'T', 19 },
+            { 'U', 20 },
+            { 'V', 21 },
+            { 'W', 22 },
+            { 'X', 23 },
+            { 'Y', 24 },
+            { 'Z', 25 }
+        };
+
+        /// <summary>
+        /// Array that maps months onto letters
+        /// </summary>
+        private static readonly char[] monthChars = { 'A', 'B', 'C', 'D', 'E', 'H', 'L', 'M', 'P', 'R', 'S', 'T' };
+
+        /// <summary>
+        /// Map used by the algoritm for odd characters
+        /// </summary>
+        private static readonly Dictionary<char, int> oddMap = new Dictionary<char, int>
+        {
+            { '0', 1 },
+            { '1', 0 },
+            { '2', 5 },
+            { '3', 7 },
+            { '4', 9 },
+            { '5', 13 },
+            { '6', 15 },
+            { '7', 17 },
+            { '8', 19 },
+            { '9', 21 },
+            { 'A', 1 },
+            { 'B', 0 },
+            { 'C', 5 },
+            { 'D', 7 },
+            { 'E', 9 },
+            { 'F', 13 },
+            { 'G', 15 },
+            { 'H', 17 },
+            { 'I', 19 },
+            { 'J', 21 },
+            { 'K', 2 },
+            { 'L', 4 },
+            { 'M', 18 },
+            { 'N', 20 },
+            { 'O', 11 },
+            { 'P', 3 },
+            { 'Q', 6 },
+            { 'R', 8 },
+            { 'S', 12 },
+            { 'T', 14 },
+            { 'U', 16 },
+            { 'V', 10 },
+            { 'W', 22 },
+            { 'X', 25 },
+            { 'Y', 24 },
+            { 'Z', 23 }
+        };
+
+        /// <summary>
         ///   Generates an Italian Fiscal Code
         /// </summary>
         /// <param name="lastName">Lastname of the holder</param>
@@ -81,12 +172,12 @@ namespace Bogus.Extensions
         }
 
         /// <summary>
-		///   This method applies the rule giving the consonants and vowels extracted by the name,
-		///   according to the algorithm.
-		/// </summary>
-		/// <param name="name">The name to process</param>
-		/// <param name="isFirstName">true, in case of first names</param>
-		/// <returns>The squeezed name</returns>
+        ///   This method applies the rule giving the consonants and vowels extracted by the name,
+        ///   according to the algorithm.
+        /// </summary>
+        /// <param name="name">The name to process</param>
+        /// <param name="isFirstName">true, in case of first names</param>
+        /// <returns>The squeezed name</returns>
 		private static string GetFiscalCodeSqueezedName(string name, bool isFirstName)
         {
             var sb = new StringBuilder();
@@ -163,96 +254,5 @@ namespace Bogus.Extensions
             var vowels = new char[] { 'A', 'E', 'I', 'O', 'U' };
             return vowels.Contains(c);
         }
-
-        /// <summary>
-        /// Map used by the algoritm for odd characters
-        /// </summary>
-        private static readonly Dictionary<char, int> oddMap = new Dictionary<char, int>
-        {
-            { '0', 1 },
-            { '1', 0 },
-            { '2', 5 },
-            { '3', 7 },
-            { '4', 9 },
-            { '5', 13 },
-            { '6', 15 },
-            { '7', 17 },
-            { '8', 19 },
-            { '9', 21 },
-            { 'A', 1 },
-            { 'B', 0 },
-            { 'C', 5 },
-            { 'D', 7 },
-            { 'E', 9 },
-            { 'F', 13 },
-            { 'G', 15 },
-            { 'H', 17 },
-            { 'I', 19 },
-            { 'J', 21 },
-            { 'K', 2 },
-            { 'L', 4 },
-            { 'M', 18 },
-            { 'N', 20 },
-            { 'O', 11 },
-            { 'P', 3 },
-            { 'Q', 6 },
-            { 'R', 8 },
-            { 'S', 12 },
-            { 'T', 14 },
-            { 'U', 16 },
-            { 'V', 10 },
-            { 'W', 22 },
-            { 'X', 25 },
-            { 'Y', 24 },
-            { 'Z', 23 }
-        };
-
-        /// <summary>
-        /// Map used by the algoritm for even characters
-        /// </summary>
-        private static readonly Dictionary<char, int> evenMap = new Dictionary<char, int>
-        {
-            { '0', 0 },
-            { '1', 1 },
-            { '2', 2 },
-            { '3', 3 },
-            { '4', 4 },
-            { '5', 5 },
-            { '6', 6 },
-            { '7', 7 },
-            { '8', 8 },
-            { '9', 9 },
-            { 'A', 0 },
-            { 'B', 1 },
-            { 'C', 2 },
-            { 'D', 3 },
-            { 'E', 4 },
-            { 'F', 5 },
-            { 'G', 6 },
-            { 'H', 7 },
-            { 'I', 8 },
-            { 'J', 9 },
-            { 'K', 10 },
-            { 'L', 11 },
-            { 'M', 12 },
-            { 'N', 13 },
-            { 'O', 14 },
-            { 'P', 15 },
-            { 'Q', 16 },
-            { 'R', 17 },
-            { 'S', 18 },
-            { 'T', 19 },
-            { 'U', 20 },
-            { 'V', 21 },
-            { 'W', 22 },
-            { 'X', 23 },
-            { 'Y', 24 },
-            { 'Z', 25 }
-        };
-
-        /// <summary>
-        /// Array that maps months onto letters
-        /// </summary>
-        private static readonly char[] monthChars = { 'A', 'B', 'C', 'D', 'E', 'H', 'L', 'M', 'P', 'R', 'S', 'T' };
     }
 }

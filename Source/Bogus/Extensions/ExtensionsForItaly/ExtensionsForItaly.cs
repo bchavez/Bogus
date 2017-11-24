47e7c1d3cc9c4ef6f84670c093998f16e946524b
supix <esposito.marce@gmail.com>
2017-11-24 19:47:00 +0100

    it-locale extension: codice fiscale
    Person class is extended with CodiceFiscale() (fiscal code)
    method. This method computes the fiscal code belonging to the
    person. The method is stable, in the sense that multiple calls
    on the same Person instance give the same code. The last part
    of the code, however is fake, since importing the actual city
    database would have been too heavy (~40kb).
    
    Finance dataset has been extended, as well. The CodiceFiscale()
    method in this case explicitly takes in input all person data
    (firstname, lastname, birthday and gender) and computes the
    corresponding code, allowing to get a fiscal code for a real
    person.
    
    The fiscal code is used in Italy to unambiguously identify individuals
    residing in
    Italy.

---
+++ Source/Bogus/Extensions/ExtensionsForItaly/ExtensionsForItaly.cs
@@ -0,0 +1,58 @@
+﻿using Bogus.DataSets;
+using System;
+
+namespace Bogus.Extensions
+{
+    /// <summary>
+    /// Italian class extensions
+    /// </summary>
+    public static class ExtensionsForItaly
+    {
+        /// <summary>
+        ///   Extends the Person class with Italian Fiscal Code
+        /// </summary>
+        /// <param name="p">The holder</param>
+        /// <param name="validChecksum">
+        ///   Indicates whether the generated Fiscal Code has a valid checksum or not
+        /// </param>
+        /// <returns>The generated Fiscal Code</returns>
+        public static string CodiceFiscale(this Person p, bool validChecksum = true)
+        {
+            return CodiceFiscaleBuilder.Build(
+                p.LastName,
+                p.FirstName,
+                p.DateOfBirth,
+                p.Gender == DataSets.Name.Gender.Male,
+                validChecksum);
+        }
+
+        /// <summary>
+        ///   Extends the Finance data set with Italian Fiscal Code and more
+        ///   fine grained parameters
+        /// </summary>
+        /// <param name="finance">An instance of the extended Finance class</param>
+        /// <param name="lastName">Lastname of the holder</param>
+        /// <param name="firstName">Firstname of the holder</param>
+        /// <param name="birthday">Birthday of the holder</param>
+        /// <param name="isMale">Indicated whether the holder is male</param>
+        /// <param name="validChecksum">
+        ///   Indicates whether the generated Fiscal Code has a valid checksum or not
+        /// </param>
+        /// <returns>The generated Fiscal Code</returns>
+        public static string CodiceFiscale(
+            this Finance finance,
+            string lastName,
+            string firstName,
+            DateTime birthday,
+            bool isMale,
+            bool validChecksum = true)
+        {
+            return CodiceFiscaleBuilder.Build(
+                lastName,
+                firstName,
+                birthday,
+                isMale,
+                validChecksum);
+        }
+    }
+}

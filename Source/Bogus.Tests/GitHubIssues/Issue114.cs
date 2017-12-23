b6185e6deeda69ed6d4bc55b5d2d00c0ffa70fb6
bchavez <bchavez@bitarmory.com>
2017-12-23 10:42:58 -0800

    Lift credit card obfuscation methods to extension methods in Bogus.Extras. Adds unit tests.

---
+++ Source/Bogus.Tests/GitHubIssues/Issue114.cs
@@ -0,0 +1,24 @@
+﻿using Bogus.DataSets;
+using Bogus.Extensions.Extras;
+using FluentAssertions;
+using Xunit;
+
+namespace Bogus.Tests.GitHubIssues
+{
+   public class Issue114 : SeededTest
+   {
+      [Fact]
+      public void should_be_able_to_get_an_obfuscated_credit_card_number()
+      {
+         var finance = new Finance();
+         finance.CreditCardNumberObfuscated().Should().Be("****-****-****-6186");
+      }
+      
+      [Fact]
+      public void cn_get_last_for_credit_card_digits()
+      {
+         var finance = new Finance();
+         finance.CreditCardNumberLastFourDigits().Should().Be("6186");
+      }
+   }
+}

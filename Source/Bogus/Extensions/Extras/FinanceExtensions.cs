2398424692bca5f9dec8ea3c21b4d753f17c0ce6
Ricardo Carneiro <rjcarneiro@users.noreply.github.com>
2017-12-23 19:01:38 +0000

    Credit card obfuscator and last four digits (#118)
    * Add method for obfuscate credit cards
    
    * Add separator and generate obfuscated credit cards just by Random.ReplaceNumbers
    
    * Added method CreditCardNumberLastFourDigits
    
    * updated obfuscator with new method name
    
    * Lift credit card obfuscation methods to extension methods in Bogus.Extras.
    Adds unit tests.
    
    * Revert changes to Finance.cs
    
    * HISTORY change log.

---
+++ Source/Bogus/Extensions/Extras/FinanceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Bogus.DataSets;
+
+namespace Bogus.Extensions.Extras
+{
+   public static class FinanceExtensions
+   {
+      /// <summary>
+      /// Generate a PCI compliant obfuscated credit card number ****-****-****-1234.
+      /// </summary>
+      /// <param name="separator">The string value to separate the obfuscated credit card.</param>
+      public static string CreditCardNumberObfuscated(this Finance f, string separator = "-")
+      {
+         separator = separator ?? string.Empty;
+
+         return f.Random.ReplaceNumbers($"****{separator}****{separator}****{separator}####");
+      }
+
+      /// <summary>
+      /// Generates the last four digits for a credit card.
+      /// </summary>      
+      public static string CreditCardNumberLastFourDigits(this Finance f)
+      {
+         return f.Random.ReplaceNumbers("####");
+      }
+   }
+}

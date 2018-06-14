e217abf8052b797a57390c34523cb4b23353bca9
bchavez <bchavez@bitarmory.com>
2018-06-14 13:34:16 -0700

    Make locale unit test checks pass. Updated README.md with Arabic locale. Ensures "ar" locale works.

---
+++ Source/Bogus.Tests/GitHubIssues/PullRequest149.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Bogus.Tests.GitHubIssues
+{
+   public class PullRequest149
+   {
+      [Fact]
+      public void can_use_arabic_locale()
+      {
+         var f = new Faker("ar");
+
+         f.Name.FirstName().Should().Be("عبير");
+      }
+   }
+}

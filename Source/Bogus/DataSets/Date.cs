492871697da3145e1f49dce5a511e12cad250419
Simon Cropp <simon.cropp@gmail.com>
2018-07-05 20:58:21 +1000

    add datetimeoffset

--- Source/Bogus/DataSets/Date.cs
+++ Source/Bogus/DataSets/Date.cs
@@ -43,6 +43,24 @@ namespace Bogus.DataSets
          return maxDate - partTimeSpan;
       }
 
+      /// <summary>
+      /// Get a <see cref="DateTimeOffset"/> in the past between <paramref name="refDate"/> and <paramref name="yearsToGoBack"/>.
+      /// </summary>
+      /// <param name="yearsToGoBack">Years to go back from <paramref name="refDate"/>. Default is 1 year.</param>
+      /// <param name="refDate">The date to start calculations. Default is <see cref="DateTimeOffset.Now"/>.</param>
+      public DateTimeOffset PastOffset(int yearsToGoBack = 1, DateTimeOffset? refDate = null)
+      {
+         var maxDate = refDate ?? DateTimeOffset.Now;
+
+         var minDate = maxDate.AddYears(-yearsToGoBack);
+
+         var totalTimeSpanTicks = (maxDate - minDate).Ticks;
+
+         var partTimeSpan = RandomTimeSpanFromTicks(totalTimeSpanTicks);
+
+         return maxDate - partTimeSpan;
+      }
+
       TimeSpan RandomTimeSpanFromTicks(long totalTimeSpanTicks)
       {
 //find % of the timespan
@@ -60,6 +78,15 @@ namespace Bogus.DataSets
       }
 
       /// <summary>
+      /// Get a <see cref="DateTimeOffset"/> that will happen soon.
+      /// </summary>
+      /// <param name="days">A date no more than N days ahead.</param>
+      public DateTimeOffset SoonOffset(int days = 1)
+      {
+         return BetweenOffset(DateTimeOffset.Now, DateTimeOffset.Now.AddDays(days));
+      }
+
+      /// <summary>
       /// Get a <see cref="DateTime"/> in the future between <paramref name="refDate"/> and <paramref name="yearsToGoForward"/>.
       /// </summary>
       /// <param name="yearsToGoForward">Years to go forward from <paramref name="refDate"/>. Default is 1 year.</param>
@@ -67,6 +94,24 @@ namespace Bogus.DataSets
       public DateTime Future(int yearsToGoForward = 1, DateTime? refDate = null)
       {
          var minDate = refDate ?? DateTime.Now;
+
+         var maxDate = minDate.AddYears(yearsToGoForward);
+
+         var totalTimeSpanTicks = (maxDate - minDate).Ticks;
+
+         var partTimeSpan = RandomTimeSpanFromTicks(totalTimeSpanTicks);
+
+         return minDate + partTimeSpan;
+      }
+
+      /// <summary>
+      /// Get a <see cref="DateTimeOffset"/> in the future between <paramref name="refDate"/> and <paramref name="yearsToGoForward"/>.
+      /// </summary>
+      /// <param name="yearsToGoForward">Years to go forward from <paramref name="refDate"/>. Default is 1 year.</param>
+      /// <param name="refDate">The date to start calculations. Default is <see cref="DateTimeOffset.Now"/>.</param>
+      public DateTimeOffset FutureOffset(int yearsToGoForward = 1, DateTimeOffset? refDate = null)
+      {
+         var minDate = refDate ?? DateTimeOffset.Now;
 
          var maxDate = minDate.AddYears(yearsToGoForward);
 
@@ -95,12 +140,46 @@ namespace Bogus.DataSets
       }
 
       /// <summary>
-      /// Get a random <see cref="DateTime"/> within the last few days since now.
+      /// Get a random <see cref="DateTimeOffset"/> between <paramref name="start"/> and <paramref name="end"/>.
+      /// </summary>
+      /// <param name="start">Starting</param>
+      /// <param name="end">Ending</param>
+      public DateTimeOffset BetweenOffset(DateTimeOffset start, DateTimeOffset end)
+      {
+         var minTicks = Math.Min(start.Ticks, end.Ticks);
+         var maxTicks = Math.Max(start.Ticks, end.Ticks);
+
+         var totalTimeSpanTicks = maxTicks - minTicks;
+
+         var partTimeSpan = RandomTimeSpanFromTicks(totalTimeSpanTicks);
+
+         return new DateTimeOffset(minTicks, DateTimeOffset.Now.Offset) + partTimeSpan;
+      }
+
+      /// <summary>
+      /// Get a random <see cref="DateTime"/> within the last few days since DateTime.Now.
       /// </summary>
       /// <param name="days">Number of days to go back.</param>
       public DateTime Recent(int days = 1)
       {
          var maxDate = DateTime.Now;
+
+         var minDate = days == 0 ? DateTime.Now.Date : maxDate.AddDays(-days);
+
+         var totalTimeSpanTicks = (maxDate - minDate).Ticks;
+
+         var partTimeSpan = RandomTimeSpanFromTicks(totalTimeSpanTicks);
+
+         return maxDate - partTimeSpan;
+      }
+
+      /// <summary>
+      /// Get a random <see cref="DateTimeOffset"/> within the last few days since <see cref="DateTimeOffset.Now"/>.
+      /// </summary>
+      /// <param name="days">Number of days to go back.</param>
+      public DateTimeOffset RecentOffset(int days = 1)
+      {
+         var maxDate = DateTimeOffset.Now;
 
          var minDate = days == 0 ? DateTime.Now.Date : maxDate.AddDays(-days);
 

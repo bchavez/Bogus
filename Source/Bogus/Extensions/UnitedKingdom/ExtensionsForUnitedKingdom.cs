35f99311dc4c3d71d9a8bd57bfd0dcff63a494c7
David Price <david@mortware.net>
2018-07-05 14:58:09 +0100

    Added valid prefix/suffix for Ninos

--- Source/Bogus/Extensions/UnitedKingdom/ExtensionsForUnitedKingdom.cs
+++ Source/Bogus/Extensions/UnitedKingdom/ExtensionsForUnitedKingdom.cs
@@ -26,17 +26,28 @@ namespace Bogus.Extensions.UnitedKingdom
       /// <summary>
       /// National Insurance Number
       /// </summary>
-      public static string Nin(this Finance finance, bool includeSeparator = true)
+      public static string Nino(this Finance finance, bool includeSeparator = true)
       {
-         const string withSeparator = "?? ## ## ## ?";
-         const string withoutSeparator = "??######?";
-         
-         if( includeSeparator )
-         {
-            return finance.Random.Replace(withSeparator);
-         }
+         string[] invalidPrefixes = { "GB", "BG", "NK", "KN", "TN", "NT", "ZZ" };
+         char[] valid1stPrefixChars = { 'A','B','C','E','G','H','J','K','L','M','N','O','P','R','S','T','W','X','Y','Z' };
+         char[] valid2ndPrefixChars = { 'A','B','C','E','G','H','J','K','L','M','N',    'P','R','S','T','W','X','Y','Z' };
+         char[] validSuffixChars = { 'A', 'B', 'C', 'D' };
 
-         return finance.Random.Replace(withoutSeparator);
+         string prefix;
+         do
+         {
+            prefix = "";
+            prefix += finance.Random.ArrayElement(valid1stPrefixChars);
+            prefix += finance.Random.ArrayElement(valid2ndPrefixChars);
+         } while (invalidPrefixes.Any(x => x.Equals(prefix)));
+
+         char suffix = finance.Random.ArrayElement(validSuffixChars);
+
+         if (includeSeparator)
+         {
+            return finance.Random.ReplaceNumbers($"{prefix} ## ## ## {suffix}");
+         }
+         return finance.Random.ReplaceNumbers($"{prefix}######{suffix}");
       }
    }
 }

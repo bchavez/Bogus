9f91f67009771b56fa8bd2a02bc8ee7902aa459b
bchavez <bchavez@bitarmory.com>
2018-10-02 11:45:04 -0700

    Add unit test for PR #180.

---
+++ Source/Bogus.Tests/GitHubIssues/PullRequest180.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using FluentAssertions;
+using Xunit;
+
+namespace Bogus.Tests.GitHubIssues
+{
+   public class PullRequest180 : IDisposable
+   {
+      private CultureInfo current;
+
+      public PullRequest180()
+      {
+         current = Thread.CurrentThread.CurrentCulture;
+      }
+
+      [Fact]
+      public void lower_invarant_should_be_used_to_avoid_locale_issues_in_json_path()
+      {
+         var turkish = CultureInfo.GetCultureInfo("tr");
+         Thread.CurrentThread.CurrentCulture = turkish;
+         var faker = new Faker("tr");
+         faker.Internet.Url().Should().NotBeNullOrWhiteSpace();
+      }
+
+      public void Dispose()
+      {
+         Thread.CurrentThread.CurrentCulture = current;
+      }
+   }
+}

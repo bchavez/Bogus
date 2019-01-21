a2c683522864235fcb1066986e03e57d8b6df475
bchavez <bchavez@bitarmory.com>
2019-01-21 11:32:48 -0800

    Re #197 - Add unit test to detect non-propagation of Randomizer.

---
+++ Source/Bogus.Tests/GitHubIssues/PullRequest197.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.CompilerServices;
+using Bogus.DataSets;
+using FluentAssertions;
+using Xunit;
+
+namespace Bogus.Tests.GitHubIssues
+{
+   public class PullRequest197
+   {
+      public class Internet2 : Internet
+      {
+         public Name DependentDataSet => this.Name;
+      }
+
+      [Fact]
+      public void ensure_randomizer_propagates_to_dependent_datasets()
+      {
+         var internet = new Internet2();
+
+         var internetRandomizer = internet.Random;
+
+         var dependentRandomizer = internet.DependentDataSet.Random;
+
+         dependentRandomizer.Should().BeSameAs(internetRandomizer);
+      }
+   }
+}

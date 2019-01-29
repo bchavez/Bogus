0b68db7fbcb9e3ea0888d853235a51e97158ac24
Bart de Bever <bartdebever112@gmail.com>
2019-01-30 00:05:13 +0100

    Chars and resource doc (#202)

--- Source/Bogus/Chars.cs
+++ Source/Bogus/Chars.cs
@@ -9,20 +9,24 @@ namespace Bogus
       /// Lower case, a-z.
       /// </summary>
       public const string LowerCase = "abcdefghijklmnopqrstuvwxyz";
+
       /// <summary>
       /// Upper case, A-Z.
       /// </summary>
       public const string UpperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
       /// <summary>
       /// Numbers, 0-9.
       /// </summary>
       public const string Numbers = "0123456789";
+
       /// <summary>
-      /// Hexadecimal, 0-9 and a-z.
+      /// Hexadecimal, 0-9 and a-f.
       /// </summary>
       public const string HexLowerCase = Numbers + "abcdef";
+
       /// <summary>
-      /// Hexadecimal, 0-9 and A-Z.
+      /// Hexadecimal, 0-9 and A-F.
       /// </summary>
       public const string HexUpperCase = Numbers + "ABCDEF";
    }
--- Source/Bogus/ResourceHelper.cs
+++ Source/Bogus/ResourceHelper.cs
@@ -11,6 +11,9 @@ namespace Bogus
       /// <summary>
       /// Checks to see if a resource exists in an assembly.
       /// </summary>
+      /// <param name="assembly">The assembly containing the resource.</param>
+      /// <param name="resourceName">The name of the resource.</param>
+      /// <returns>A boolean indicating if the resource exists in the assembly.</returns>
       public static bool ResourceExists(System.Reflection.Assembly assembly, string resourceName)
       {
          return assembly.GetManifestResourceInfo(resourceName) != null;
@@ -19,6 +22,9 @@ namespace Bogus
       /// <summary>
       /// Reads a byte[] resource from an assembly.
       /// </summary>
+      /// <param name="assembly">The assembly containing the resource.</param>
+      /// <param name="resourceName">The name of the resource.</param>
+      /// <returns>The resource in bytes.</returns>
       public static byte[] ReadResource(System.Reflection.Assembly assembly, string resourceName)
       {
          using( var s = assembly.GetManifestResourceStream(resourceName) )
@@ -33,6 +39,9 @@ namespace Bogus
       /// <summary>
       /// Reads a BSON <see cref="BValue"/> resource from an assembly.
       /// </summary>
+      /// <param name="assembly">The assembly containing the resource.</param>
+      /// <param name="resourceName">The name of the resource.</param>
+      /// <returns>The value of the resource as a <see cref="BValue"/> object.</returns>
       public static BValue ReadBValueResource(System.Reflection.Assembly assembly, string resourceName)
       {
          using( var s = assembly.GetManifestResourceStream(resourceName) )
@@ -47,6 +56,9 @@ namespace Bogus
       /// <summary>
       /// Reads a BSON <see cref="BObject"/> resource from an assembly.
       /// </summary>
+      /// <param name="assembly">The assembly containing the resource.</param>
+      /// <param name="resourceName">The name of the resource.</param>
+      /// <returns>The value of the resource as a <see cref="BObject"/> object.</returns>
       public static BObject ReadBObjectResource(System.Reflection.Assembly assembly, string resourceName)
       {
          using( var s = assembly.GetManifestResourceStream(resourceName) )

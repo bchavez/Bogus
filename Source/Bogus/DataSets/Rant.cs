bd9a2a3525de8af8377f24339b5376a113cd94c5
Bart de Bever <bartdebever112@gmail.com>
2019-01-29 22:40:49 +0100

    Documentation for the Rant.cs class.

--- Source/Bogus/DataSets/Rant.cs
+++ Source/Bogus/DataSets/Rant.cs
@@ -10,6 +10,8 @@ namespace Bogus.DataSets
       /// <summary>
       /// Generates a random user review.
       /// </summary>
+      /// <param name="product">The name of the product.</param>
+      /// <returns>A value indicating a user review.</returns>
       public string Review(string product = "product")
       {
          return this.GetRandomArrayItem("review")
@@ -19,6 +21,9 @@ namespace Bogus.DataSets
       /// <summary>
       /// Generate an array of random reviews.
       /// </summary>
+      /// <param name="product">The name of the product</param>
+      /// <param name="lines">The amount of lines wanting to be generated.</param>
+      /// <returns>An array of strings with it's value representing reviews</returns>
       public string[] Reviews(string product = "product", int lines = 2)
       {
          return Enumerable.Range(1, lines)

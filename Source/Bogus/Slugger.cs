af8b5e84e5a813f148032f4da78c6bd886224032
bchavez <bchavez@bitarmory.com>
2019-06-22 20:51:09 -0700

    better slugger support.

---
+++ Source/Bogus/Slugger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Bogus
+{
+   /// <summary>
+   /// An advanced utility class to slugify text. A port of https://github.com/pid/speakingurl
+   /// </summary>
+   public static class Slugger
+   {
+      public static Dictionary<char, char> charMap = new Dictionary<char, char>()
+         {
+
+         };
+
+      public static string GetSlug(
+         string input,
+         string separator = "-",
+         string lang = "en",
+         bool symbols = true,
+         bool maintainCase = false,
+         bool titleCase = false,
+         int? truncate = null,
+         bool uric = false,
+         bool uricNoSlash = false,
+         bool mark = false
+      )
+      {
+
+      }
+   }
+}

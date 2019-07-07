3f3c1d91a7c22bc1d0a8d2a39f712e156b66e13c
Colin Mackay <github@colinmackay.scot>
2019-07-07 15:38:34 +0100

    Set global randomizer in GB reg plate tests

--- Source/Bogus.Tests/ExtensionTests/RegistrationPlateExtensionsForGreatBritainTests.cs
+++ Source/Bogus.Tests/ExtensionTests/RegistrationPlateExtensionsForGreatBritainTests.cs
@@ -67,50 +67,48 @@ namespace Bogus.Tests.ExtensionTests
            var plate = vehicle.GbRegistrationPlate(new DateTime(2009, 9, 1), new DateTime(2009, 12, 31));
            plate.Substring(2, 2).Should().Be("59");
        }
-//
-//       [Fact]
-//       public void new_licence_plate_on_each_generate()
-//       {
-//           var vehicle = new Vehicle();
-//           vehicle.Random = new Randomizer(1234);
-//
-//           var plates = Enumerable
-//               .Range(1,10)
-//               .Select(_ => vehicle.GbRegistrationPlate(new DateTime(2001, 9, 1), new DateTime(2019, 7, 5)))
-//               .ToArray();
-//
-//           testOutputHelper.WriteLine(string.Join(Environment.NewLine, plates));
-//           plates.Distinct().Count().Should().Be(plates.Length);
-//       }
-//
-//       [Fact]
-//       public void edinburgh_mid2007_plates_have_exception_applied_to_them()
-//       {
-//          var vehicle = new Vehicle();
-//          vehicle.Random = new Randomizer(293);
-//          var plate = vehicle.GbRegistrationPlate(new DateTime(2007, 3, 1), new DateTime(2007, 8, 31));
-//          plate.Should().StartWith("TN07");
-//       }
-//
-//       [Theory]
-//       [InlineData(1, "XE68")]
-//       [InlineData(2, "XF68")]
-//       [InlineData(3, "XA19")]
-//       [InlineData(4, "XB19")]
-//       [InlineData(5, "XC19")]
-//       [InlineData(6, "XD19")]
-//       [InlineData(7, "XE19")]
-//       [InlineData(8, "XF19")]
-//       [InlineData(9, "XA69")]
-//       [InlineData(10, "XB69")]
-//       [InlineData(11, "XC69")]
-//       [InlineData(12, "XD69")]
-//       public void export_plates_have_pseudo_location_marker_based_on_registration_date(int month, string partialPlate)
-//       {
-//          var vehicle = new Vehicle();
-//          vehicle.Random = new Randomizer(14);
-//          var plate = vehicle.GbRegistrationPlate(new DateTime(2019, month, 1), new DateTime(2019, month, DateTime.DaysInMonth(2019, month)));
-//          plate.Should().StartWith(partialPlate);
-//       }
+
+       [Fact]
+       public void new_licence_plate_on_each_generate()
+       {
+           var vehicle = new Vehicle();
+           var plates = Enumerable
+               .Range(1,10)
+               .Select(_ => vehicle.GbRegistrationPlate(new DateTime(2001, 9, 1), new DateTime(2019, 7, 5)))
+               .ToArray();
+
+           testOutputHelper.WriteLine(string.Join(Environment.NewLine, plates));
+           plates.Distinct().Count().Should().Be(plates.Length);
+       }
+
+       [Fact]
+       public void edinburgh_mid2007_plates_have_exception_applied_to_them()
+       {
+          Randomizer.Seed = new Random(293);
+          var vehicle = new Vehicle();
+          var plate = vehicle.GbRegistrationPlate(new DateTime(2007, 3, 1), new DateTime(2007, 8, 31));
+          plate.Should().StartWith("TN07");
+       }
+
+       [Theory]
+       [InlineData(1, "XE68")]
+       [InlineData(2, "XF68")]
+       [InlineData(3, "XA19")]
+       [InlineData(4, "XB19")]
+       [InlineData(5, "XC19")]
+       [InlineData(6, "XD19")]
+       [InlineData(7, "XE19")]
+       [InlineData(8, "XF19")]
+       [InlineData(9, "XA69")]
+       [InlineData(10, "XB69")]
+       [InlineData(11, "XC69")]
+       [InlineData(12, "XD69")]
+       public void export_plates_have_pseudo_location_marker_based_on_registration_date(int month, string partialPlate)
+       {
+          Randomizer.Seed = new Random(14);
+          var vehicle = new Vehicle();
+          var plate = vehicle.GbRegistrationPlate(new DateTime(2019, month, 1), new DateTime(2019, month, DateTime.DaysInMonth(2019, month)));
+          plate.Should().StartWith(partialPlate);
+       }
    }
 }

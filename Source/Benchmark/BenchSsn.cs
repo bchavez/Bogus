75ac7844d72f3192432b1311420050b8389b025d
bchavez <bchavez@bitarmory.com>
2019-10-12 08:52:04 -0700

    Benchmark various solutions to generating a new SSN; saving work for now. #260

---
+++ Source/Benchmark/BenchSsn.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Attributes.Exporters;
+using Bogus;
+
+namespace Benchmark
+{
+   [RPlotExporter]
+   public class BenchSsn
+   {
+      private Randomizer r;
+
+      [GlobalSetup]
+      public void Setup()
+      {
+         r = new Randomizer();
+      }
+
+      [Benchmark]
+      public void SsnAlgo1()
+      {
+         var a = r.Int(1, 898);
+         var b = r.Int(1, 99);
+         var c = r.Int(1, 9999);
+
+         var result = $"{a:000}-{b:00}-{c:0000}";
+      }
+
+      [Benchmark]
+      public void SsnAlgo2()
+      {
+         var a = r.Int(1, 898);
+         var b = r.Int(1, 99);
+         var c = r.Int(1, 9999);
+
+         var result = string.Format("{0:000}-{1:00}-{2:0000}", a, b, c);
+      }
+
+      [Benchmark]
+      public void SsnAlgo3()
+      {
+         // 898 = 0b1110000010
+
+         var x = r.Int();
+
+         var a = x & 0xFFC00000 >> 22;
+
+         var b = x & 0x003F ;
+
+         var c = (x << 10 + 7 + 10);
+
+
+      }
+   }
+}
---
+++ Source/Bogus.Tests/GitHubIssues/Issue260.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Bogus.Tests.GitHubIssues
+{
+   public class Issue260 : SeededTest
+   {
+      private readonly ITestOutputHelper console;
+
+      public Issue260(ITestOutputHelper console)
+      {
+         this.console = console;
+      }
+
+      [Fact]
+      public void generate_new_ssn()
+      {
+         var r = new Randomizer();
+
+         var x = r.Int();
+
+         var a = (x << 10) & 0b1111_1111_10;
+
+         var b = (x << 10 + 7);
+
+         var c = (x << 10 + 7 + 10);
+
+         console.Dump(Convert.ToString(x, 2));
+         console.Dump(Convert.ToString(a, 2));
+         console.Dump(Convert.ToString(b, 2));
+         console.Dump(Convert.ToString(c, 2));
+
+         var result = $"{a:000}-{b:00}-{c:0000}";
+
+         console.Dump(result);
+      }
+   }
+}

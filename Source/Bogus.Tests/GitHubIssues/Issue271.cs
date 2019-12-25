937cecda19b24a85190ab56479166b1b6050389b
bchavez <bchavez@bitarmory.com>
2019-12-25 11:26:48 -0800

    Issue #271 - Forgot to add this unit test sitting on my local drive.

---
+++ Source/Bogus.Tests/GitHubIssues/Issue271.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using Bogus.Extensions.Brazil;
+using FluentAssertions;
+
+namespace Bogus.Tests.GitHubIssues
+{
+   public class Issue271 : SeededTest
+   {
+      [Fact]
+      public void brazil_cpf_format_parameter_should_respect_person_context()
+      {
+         var p = new Bogus.Person();
+
+         p.Cpf().Should().Be("869.287.971-18");
+         p.Cpf(includeFormatSymbols:false).Should().Be("86928797118");
+      }
+   }
+}

749aa8aa36354a4c9ac552259840e20efd95acbc
bchavez <bchavez@bitarmory.com>
2020-08-13 18:52:33 -0700

    Add unit test for #318

---
+++ Source/Bogus.Tests/GitHubIssues/Issue318.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Bogus.Extensions.UnitedKingdom;
+using FluentAssertions;
+
+namespace Bogus.Tests.GitHubIssues
+{
+   public class Issue318 : SeededTest
+   {
+      [Fact]
+      public void can_get_country_of_uk()
+      {
+         var f = new Faker("en_GB");
+         var country = f.Address.CountryOfUnitedKingdom();
+         country.Should().Be("Wales");
+      }
+
+      [Fact]
+      public void can_get_country_of_uk_without_locale_specified()
+      {
+         var f = new Faker();
+         var country = f.Address.CountryOfUnitedKingdom();
+         country.Should().Be("Wales");
+      }
+   }
+}

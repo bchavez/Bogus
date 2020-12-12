c429ae9ba87a98816b39441d519c827ee5e7b1d2
bchavez <bchavez@bitarmory.com>
2020-12-12 10:39:08 -0800

    Add unit test for #342.

---
+++ Source/Bogus.Tests/GitHubIssues/Issue342.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Bogus.DataSets;
+using FluentAssertions;
+using Xunit;
+
+namespace Bogus.Tests.GitHubIssues
+{
+   public class Issue342 : SeededTest
+   {
+      [Fact]
+      public void more_realistic_pt_BR_city_name()
+      {
+         var a = new Address("pt_BR");
+         a.City().Should().Be("Pelotas");
+      }
+   }
+}

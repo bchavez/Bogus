8a2934d48c2dcef3cd0e4f51cb3cec8e1aa16080
David Maltby <david.maltby@hotmail.com>
2022-02-20 08:36:13 -0600

    Using C# XMLdoc markup for IRandom interface

--- Source/Bogus/IRandom.cs
+++ Source/Bogus/IRandom.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System;
+
 namespace Bogus
 {
    /// <summary>
@@ -6,57 +7,43 @@ namespace Bogus
    /// </summary>
    public interface IRandom
    {
-      //
-      // Summary:
-      //     Returns a non-negative random integer.
-      //
-      // Returns:
-      //     A 32-bit signed integer that is greater than or equal to 0 and less than System.Int32.MaxValue.
+      /// <summary>
+      /// Returns a non-negative random integer.
+      /// </summary>
+      /// <returns>A 32-bit signed integer that is greater than or equal to 0 and less than <seealso cref="System.Int32.MaxValue"/>.
+      /// </returns>
       int Next();
 
-      //
-      // Summary:
-      //     Returns a random integer that is within a specified range.
-      //
-      // Parameters:
-      //   minValue:
-      //     The inclusive lower bound of the random number returned.
-      //
-      //   maxValue:
-      //     The exclusive upper bound of the random number returned. maxValue must be greater
-      //     than or equal to minValue.
-      //
-      // Returns:
-      //     A 32-bit signed integer greater than or equal to minValue and less than maxValue;
-      //     that is, the range of return values includes minValue but not maxValue. If minValue
-      //     equals maxValue, minValue is returned.
-      //
-      // Exceptions:
-      //   T:System.ArgumentOutOfRangeException:
-      //     minValue is greater than maxValue.
+      /// <summary>
+      /// Returns a random integer that is within a specified range.
+      /// </summary>
+      /// <param name="minValue">The inclusive lower bound of the random number returned.</param>
+      /// <param name="maxValue">
+      /// The exclusive upper bound of the random number returned. <paramref name="maxValue"/> 
+      /// must be greater than or equal to <paramref name="minValue"/>.
+      /// </param>
+      /// <returns>
+      /// A 32-bit signed integer greater than or equal to <paramref name="minValue"/> and less than 
+      /// <paramref name="maxValue"/>; that is, the range of return values includes 
+      /// <paramref name="minValue"/> but not <paramref name="maxValue"/>. If <paramref name="minValue"/>
+      /// equals <paramref name="maxValue"/>, <paramref name="minValue"/> is returned.
+      /// </returns>
+      /// <exception cref="ArgumentOutOfRangeException">
+      /// When <paramref name="minValue"/> is greater than <paramref name="maxValue"/>.
+      /// </exception>
       int Next(int minValue, int maxValue);
 
-      //
-      // Summary:
-      //     Fills the elements of a specified array of bytes with random numbers.
-      //
-      // Parameters:
-      //   buffer:
-      //     An array of bytes to contain random numbers.
-      //
-      // Exceptions:
-      //   T:System.ArgumentNullException:
-      //     buffer is null.
+      /// <summary>
+      /// Fills the elements of a specified array of bytes with random numbers.
+      /// </summary>
+      /// <param name="buffer">An array of bytes to contain random numbers.</param>
+      /// <exception cref="ArgumentNullException">When buffer is null.</exception>
       void NextBytes(byte[] buffer);
 
-      //
-      // Summary:
-      //     Returns a random floating-point number that is greater than or equal to 0.0,
-      //     and less than 1.0.
-      //
-      // Returns:
-      //     A double-precision floating point number that is greater than or equal to 0.0,
-      //     and less than 1.0.
+      /// <summary>
+      /// Returns a random floating-point number that is greater than or equal to 0.0, and less than 1.0.
+      /// </summary>
+      /// <returns>A double-precision floating point number that is greater than or equal to 0.0, and less than 1.0.</returns>
       double NextDouble();
    }
 }

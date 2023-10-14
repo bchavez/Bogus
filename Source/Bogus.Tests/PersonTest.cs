30c48cb4b2f0d7397c61b5051763fa938575a91d
Patrick Garcia <garcipat87@gmail.com>
2023-10-14 19:09:03 +0200

    removed unused property again

--- Source/Bogus.Tests/PersonTest.cs
+++ Source/Bogus.Tests/PersonTest.cs
@@ -28,7 +28,6 @@ namespace Bogus.Tests
          public string FirstName { get; set; }
          public string Email { get; set; }
          public string LastName { get; set; }
-         public DateTime BirthDay { get; set; }
       }
 
       [Fact]

ab0a8328cd92cc1f5f47298cb1c7f538aaaa8a84
Patrick Garcia <garcipat87@gmail.com>
2023-10-14 18:40:43 +0200

    fix errors and add other test

--- Source/Bogus.Tests/PersonTest.cs
+++ Source/Bogus.Tests/PersonTest.cs
@@ -28,6 +28,7 @@ namespace Bogus.Tests
          public string FirstName { get; set; }
          public string Email { get; set; }
          public string LastName { get; set; }
+         public DateTime BirthDay { get; set; }
       }
 
       [Fact]

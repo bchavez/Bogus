110f3430f0558b0b3d88b277a499c042de919c56
p1971 <pete@madhippy.com>
2024-01-13 06:08:01 +0000

    Added support for UK VAT registration numbers. (#510)
    * Added support for UK VAT registration numbers.
    
    * Fix minor code formatting indentation
    
    ---------
    
    Co-authored-by: Brian Chavez <bchavez@bitarmory.com>

--- Source/Bogus/Extensions/UnitedKingdom/ExtensionsForUnitedKingdom.cs
+++ Source/Bogus/Extensions/UnitedKingdom/ExtensionsForUnitedKingdom.cs
@@ -1,3 +1,5 @@
+using System;
+
 using Bogus.Bson;
 using Bogus.DataSets;
 
@@ -67,5 +69,86 @@ public static class ExtensionsForUnitedKingdom
    {
       var countries = Database.Get(nameof(address), "uk_country", "en_GB") as BArray;
       return address.Random.ArrayElement(countries);
+   }
+
+   /// <summary>
+   /// Generates a UK compatible VAT registration number.
+   /// </summary>
+   /// <param name="finance">A reference to <see cref="Finance"/>.</param>
+   /// <param name="registrationNumberType">The type of vat registration number to generate.</param>
+   /// <param name="includeSeparator">Indicates the resultant string should be spaced according to VAT registration formatting defaults.</param>
+   /// <returns>A string representation of a VAT registration number.</returns>
+   /// <exception cref="NotImplementedException">Exception thrown if the requested registration number is not supported.</exception>
+   public static string VatNumber(this Finance finance, VatRegistrationNumberType registrationNumberType, bool includeSeparator = true)
+   {
+      int min = 0;
+      int max = 9_999_999;
+      switch( registrationNumberType )
+      {
+         case VatRegistrationNumberType.HealthAuthority:
+            min = 500;
+            max = 999;
+            break;
+         case VatRegistrationNumberType.GovernmentDepartment:
+            min = 0;
+            max = 499;
+            break;
+      }
+
+      int random = finance.Random.Int(min, max);
+
+      switch( registrationNumberType )
+      {
+         case VatRegistrationNumberType.HealthAuthority:
+            return includeSeparator
+               ? $"GB HA {random:000}"
+               : $"GBHA{random:000}";
+         case VatRegistrationNumberType.GovernmentDepartment:
+            return includeSeparator
+               ? $"GB GD {random:000}"
+               : $"GBGD{random:000}";
+         case VatRegistrationNumberType.Standard:
+            int checksum = CalculateChecksum(random);
+            return includeSeparator
+               ? $"GB {random:000 0000} {checksum:00}"
+               : $"GB{random:0000000}{checksum:00}";
+         case VatRegistrationNumberType.BranchTrader:
+            checksum = CalculateChecksum(random);
+            string suffix = finance.Random.ReplaceNumbers("###");
+            return includeSeparator
+               ? $"GB {random:000 0000} {checksum:00} {suffix}"
+               : $"GB{random:0000000}{checksum:00}{suffix}";
+         default:
+            throw new NotImplementedException($"[{registrationNumberType}] is not a supported [{nameof(VatRegistrationNumberType)}].");
+      }
+   }
+
+   private static int CalculateChecksum(int n)
+   {
+      int total = 0;
+      int divisor = 1;
+
+      while( n / divisor >= 10 )
+      {
+         divisor *= 10;
+      }
+
+      int index = 0;
+      while( divisor > 0 )
+      {
+         int digit = n / divisor;
+         total += digit * (8 - index);
+
+         n %= divisor;
+         divisor /= 10;
+         index++;
+      }
+
+      while( total > 0 )
+      {
+         total -= 97;
+      }
+
+      return Math.Abs(total);
    }
 }
